d693b7a9b2d89733d96bb60a44161d328bcb76e9
Akhil-star <akhilakki1305@gmail.com>
2020-10-04 23:37:27 +0530

    First Name Pattern

--- src/main/java/com/cg/userregistration/UserRegistration.java
+++ src/main/java/com/cg/userregistration/UserRegistration.java
@@ -3,10 +3,19 @@ package com.cg.userregistration;
 import java.util.*;
 
 public class UserRegistration {
-
+	public static void firstName(String firstName) {
+		String regex = "^[A-Z]{1}[A-Za-z]{2,}$";
+		if (firstName.matches(regex))
+			System.out.println("First Name is Valid");
+		else
+			System.out.println("First Name is Invalid");
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcome to user registration problem");
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the First Name");
+		String fName = sc.next();
+		firstName(fName);
+		sc.close();
 	}
 
 }
