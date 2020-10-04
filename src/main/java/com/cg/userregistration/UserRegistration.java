9949f86e920038c3b68aef3d5410ea8a630b872b
Akhil-star <akhilakki1305@gmail.com>
2020-10-04 23:41:30 +0530

    Last name pattern

--- src/main/java/com/cg/userregistration/UserRegistration.java
+++ src/main/java/com/cg/userregistration/UserRegistration.java
@@ -3,18 +3,20 @@ package com.cg.userregistration;
 import java.util.*;
 
 public class UserRegistration {
-	public static void firstName(String firstName) {
+	
+	public static void lastName(String lastName) {
 		String regex = "^[A-Z]{1}[A-Za-z]{2,}$";
-		if (firstName.matches(regex))
-			System.out.println("First Name is Valid");
+		if (lastName.matches(regex))
+			System.out.println("Last Name is Valid");
 		else
-			System.out.println("First Name is Invalid");
+			System.out.println("Last Name is Invalid");
 	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the First Name");
-		String fName = sc.next();
-		firstName(fName);
+		System.out.println("Enter the Last Name");
+		String lName = sc.next();
+		lastName(lName);
 		sc.close();
 	}
 
