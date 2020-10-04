e4c3ab959840889486bb7a0718050f07ca858c8e
Akhil-star <akhilakki1305@gmail.com>
2020-10-04 23:48:10 +0530

    Email pattern validate

--- src/main/java/com/cg/userregistration/UserRegistration.java
+++ src/main/java/com/cg/userregistration/UserRegistration.java
@@ -4,19 +4,18 @@ import java.util.*;
 
 public class UserRegistration {
 	
-	public static void lastName(String lastName) {
-		String regex = "^[A-Z]{1}[A-Za-z]{2,}$";
-		if (lastName.matches(regex))
-			System.out.println("Last Name is Valid");
+	public static void email(String emailId) {
+		String regex = "^[a-z0-9+_.-]+@[a-z0-9.-]+$";
+		if (emailId.matches(regex))
+			System.out.println("EmailId is Valid");
 		else
-			System.out.println("Last Name is Invalid");
+			System.out.println("EmailId is Invalid");
 	}
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Last Name");
-		String lName = sc.next();
-		lastName(lName);
+		System.out.println("Enter the EmailId");
+		String emailId = sc.nextLine();
+		email(emailId);
 		sc.close();
 	}
 
