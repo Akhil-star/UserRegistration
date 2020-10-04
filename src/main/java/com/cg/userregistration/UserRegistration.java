07ba64a88c1323c14b4b218b88040c866cee2d03
Akhil-star <akhilakki1305@gmail.com>
2020-10-04 23:31:13 +0530

    Welcome msg

---
+++ src/main/java/com/cg/userregistration/UserRegistration.java
@@ -0,0 +1,12 @@
+package com.cg.userregistration;
+
+import java.util.*;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to user registration problem");
+
+	}
+
+}
